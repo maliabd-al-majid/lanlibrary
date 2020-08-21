76c8a5db2fc848a039348539a4d25de5dc10313f
mohamednadeem <maliabd-al-majid@fcih1.com>
2020-08-21 13:37:25 +0200

    - adding publish.gradle.

---
+++ lanlibrary/src/main/java/com/example/lanlibrary/calculation.kt
@@ -0,0 +1,8 @@
+package com.example.lanlibrary
+
+
+class calculation{
+    fun calculate(x:Int,y: Int):Int{
+        return x+ y
+    }
+}
--- lanlibrary/src/main/java/com/example/lanlibrary/lanlibrary.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lanlibrary
-
-data class twonumbers(var x:Int,var y:Int)
-class calculation{
-    fun calculate(twonumbers: twonumbers):Int{
-        return twonumbers.x+ twonumbers.y
-    }
-}
